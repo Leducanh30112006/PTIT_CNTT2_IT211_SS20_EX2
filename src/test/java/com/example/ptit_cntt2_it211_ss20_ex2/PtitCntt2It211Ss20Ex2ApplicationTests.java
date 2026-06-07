f3513f15de33e2f3d2852fb294e584b96dc26395
Leducanh30112006 <le1duc2anh3@gmail.com>
2026-06-07 12:09:49 +0700

    Initial commit

---
+++ src/test/java/com/example/ptit_cntt2_it211_ss20_ex2/PtitCntt2It211Ss20Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ptit_cntt2_it211_ss20_ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtitCntt2It211Ss20Ex2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
